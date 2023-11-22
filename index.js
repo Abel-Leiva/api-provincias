b58850af7540c85376a3906fef85bb500109363d
Abel Leiva <a.ignacio.leivaleiva@gmail.com>
2023-11-22 02:59:15 -0300

    configuracion de cors

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+const express = require("express");
+const cors = require("cors");
 const provincias = require("./src/provincias/indexProvincias");
 
-const express = require("express");
-
 const app = express();
-
 const port = 3000;
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/provincias", (req, res) => {
   try {
     let nombresProvincias = Object.keys(provincias);
-    console.log(nombresProvincias);
     res.json(nombresProvincias);
   } catch (error) {
-    console.error(error);
     res.status(500).send("Ocurrió un error al obtener las provincias");
   }
 });
+
 app.get("/provincias/:provincia", (req, res) => {
-  console.log(req.params.provincia);
   const { provincia } = req.params;
   if (req.params.provincia && provincias[provincia]) {
     res.json(provincias[provincia]);
